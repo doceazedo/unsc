9387e9380996cc5754b745d9defb246c5760097a
DoceAzedo <me@doceazedo.com>
2024-08-09 15:29:29 -0300

    Add /users/:user and /users/:user/followers endpoints

---
+++ server/routes/users/[username]/followers.ts
@@ -0,0 +1,41 @@
+export default eventHandler(async ({ context }) => {
+  const { username } = context.params;
+
+  try {
+    const resp = await fetch(
+      `https://api-v2.soundcloud.com/users/${username}/followers?client_id=nFddmw3ZibOug7XKUPPyXjYCElJCcGcv&limit=9999&offset=0&linked_partitioning=1&app_version=1723035748&app_locale=en`,
+      {
+        headers: {
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-GB,en;q=0.9",
+          "cache-control": "no-cache",
+          pragma: "no-cache",
+          "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+        referrer: "https://soundcloud.com/",
+        referrerPolicy: "origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "omit",
+      }
+    );
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 503, message: "SoundCloud request error" }),
+      {
+        status: 503,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+});
---
+++ server/routes/users/[username]/index.ts
@@ -0,0 +1,53 @@
+import { JSDOM } from "jsdom";
+
+export default eventHandler(async ({ context }) => {
+  const { username } = context.params;
+
+  try {
+    const resp = await fetch(`https://soundcloud.com/${username}`, {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-GB,en;q=0.9",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "upgrade-insecure-requests": "1",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+    });
+    const html = await resp.text();
+    const dom = new JSDOM(html);
+
+    let parsed = {};
+    const data = JSON.parse(
+      [...dom.window.document.querySelectorAll("script")]
+        .map((x) => x.innerHTML)
+        .find((x) => x.startsWith("window.__sc_hydration = "))
+        .replace("window.__sc_hydration = ", "")
+        .replace(";", "")
+    );
+    data.map((x) => (parsed[x.hydratable] = x.data));
+
+    return parsed;
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 503, message: "SoundCloud request error" }),
+      {
+        status: 503,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+});
